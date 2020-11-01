4c7f116e6b8485ea78f668ae34e25e6ae142575a
melissash <62385918+melissash@users.noreply.github.com>
2020-11-01 23:22:59 +0200

    Comments added

--- MLBackend/lbptest/MLbackend.py
+++ MLBackend/lbptest/MLbackend.py
@@ -8,9 +8,9 @@ from tqdm import tqdm
 import glob
 import pickle
 import base64
+import warnings
 
 from skimage import feature
-import warnings
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
@@ -22,7 +22,7 @@ from sklearn.metrics import classification_report, accuracy_score, recall_score,
 
 # Read the input image
 def locateFace(img):
-    # Load the cascade
+    # Load the opencv cascade for face detection
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
     # Convert into grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -30,6 +30,7 @@ def locateFace(img):
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     # Draw rectangle around the faces
     
+    #Get coordinates of every face 
     for (x, y, w, h) in faces:
         cv2.rectangle(np.copy(img), (x, y), (x+w, y+h), (255, 0, 0), 2)
     if faces == ():
@@ -62,7 +63,7 @@ class LocalBinaryPatterns:
 
 def train(X_young,y_young, X_old, y_old, k_cross_validation_ratio, testing_size, optimal_k=True, min_range_k=1, max_range_k=100, progressObject=None, jobId=-1, model_name="pretrained_knn_model"):
 
-    #X0_train, X_test, y0_train, y_test = train_test_eq_split(X, y, n_per_class=2500, random_state=123)
+    #Takes both X_young and X_old: dataset is split into 2 different categories to have a balanced training dataset
 
     X0_young_train, X0_young_test, y0_young_train, y0_young_test= train_test_split(X_young, y_young, test_size=testing_size, random_state=7)
     X0_old_train, X0_old_test, y0_old_train, y0_old_test= train_test_split(X_old, y_old, test_size=testing_size, random_state=7)
@@ -76,17 +77,24 @@ def train(X_young,y_young, X_old, y_old, k_cross_validation_ratio, testing_size,
     X0_train = df_train.drop(['age_new'], axis=1)
     y0_train = df_train['age_new']
     
-
+    #If dataset is not balanced: 1 of the classes exceeds the other by 2 or more instances
+    # warn the user that the results could be biased
     if(np.abs(pd.DataFrame(X0_young_train).ndim - pd.DataFrame(X0_old_train).ndim) >2 ):
-        raise ValueError('Error! Biased Training dataset!')
+        warnings.warn('Warning! Biased Training dataset! Your results might not be accurate.')
     
     eval_score_list = []
 
+    #Finding the range of values for the optimal k
     if optimal_k and min_range_k>0 and max_range_k>min_range_k:
+        #If the user asked to search for the optimal k and specified a valid range of values, search for the optimal k in this range
         k_range= range(min_range_k, min(max_range_k, X0_train.shape[0]))
     elif optimal_k:
+        #If the user only asked for the optimal k without specifying a range of values or if the specified range is not valid
+        #Search for k in the default range: [1, max_k] where max_k is either the shape of the training dataset if the dataset is 
+        #smaller than 50 instances or (if X_train.shape[0]>50) max_k = 50
         k_range=range(1,min(50, X0_train.shape[0]))
     else:
+        #If the user did not request to use the optimal k, use the value specified.
         k_range=[min_range_k]
     
 
@@ -95,20 +103,29 @@ def train(X_young,y_young, X_old, y_old, k_cross_validation_ratio, testing_size,
 
     #finding the optimal nb of neighbors
     for k in tqdm(k_range):
+        #create a KNN model for every k in this range
         knn = KNeighborsClassifier(n_neighbors=k, weights = 'distance',algorithm='ball_tree', p=3)
+        #fit the model on the training data
         knn.fit(X0_train, y0_train)
+        #predict a value with this model 
         y_pred = knn.predict(X_test)
+        #get its accuracy score and append it to the array 
         scores[k] = metrics.accuracy_score(y_test, y_pred)
         scores_list.append(metrics.accuracy_score(y_test, y_pred))
         if progressObject != None:
             progressObject['jobProgress'][jobId] = min((k_range.index(k)+1.0)/len(k_range), 0.99)
     
+    #the optimal k is the one that has the highest score in the array created above: score_list
     k_optimal = scores_list.index(max(scores_list))+min_range_k
+    #create the model that will be used with k = k_optimal, specify that the distance is weighted and not uniform
+    #p=3 : minkowski distance
     model = KNeighborsClassifier(n_neighbors= k_optimal, weights = 'distance', algorithm='ball_tree', p=3)
     
     accuracys=[]
 
+    #Repeated k-fold validation
     nb_splits=10
+    #specifying the classifier with the required nb of splits
     skf = StratifiedKFold(n_splits=10, random_state=None)
     skf.get_n_splits(X0_train, y0_train)
 
@@ -120,6 +137,8 @@ def train(X_young,y_young, X_old, y_old, k_cross_validation_ratio, testing_size,
     young_train_y=[]
     young_eval_x = []
     young_eval_y =[]
+
+    #Get the required range in the old instances dataframe and append them into 4 different arrays: train_old, eval_old for each X and y
     for train_old_index, test_old_index in skf.split(X0_old_train, y0_old_train):
         X_train_old, X_eval_old = pd.DataFrame(X0_old_train).iloc[train_old_index], pd.DataFrame(X0_old_train).iloc[test_old_index]
         y_train_old, y_eval_old = pd.DataFrame(y0_old_train).iloc[train_old_index], pd.DataFrame(y0_old_train).iloc[test_old_index]
@@ -128,6 +147,8 @@ def train(X_young,y_young, X_old, y_old, k_cross_validation_ratio, testing_size,
         old_train_y.append(y_train_old)
         old_eval_x.append(X_eval_old)
         old_eval_y.append(y_eval_old)
+
+    #Get the required ranges in the young instances dataframe and append them into 4 different arrays: train_young, eval_young for each X and y
     for train_young_index, test_young_index in skf.split(X0_young_train, y0_young_train):
         X_train_young, X_eval_young = pd.DataFrame(X0_young_train).iloc[train_young_index], pd.DataFrame(X0_young_train).iloc[test_young_index]
         y_train_young, y_eval_young = pd.DataFrame(y0_young_train).iloc[train_young_index], pd.DataFrame(y0_young_train).iloc[test_young_index]
@@ -136,39 +157,45 @@ def train(X_young,y_young, X_old, y_old, k_cross_validation_ratio, testing_size,
         young_eval_x.append(X_eval_young)
         young_eval_y.append(y_eval_young)
 
-    
+    #append both training and evaluation instances of the young and old dataframes into a single dataframe (using indexing)
     for i in range(nb_splits):
         X_train = pd.DataFrame(old_train_X[i]).append(pd.DataFrame(young_train_X[i]))
         X_eval = pd.DataFrame(old_eval_x[i]).append(pd.DataFrame(young_eval_x[i]))
         y_train = pd.DataFrame(old_train_y[i]).append(pd.DataFrame(young_train_y[i]))
         y_eval = pd.DataFrame(old_eval_y[i]).append(pd.DataFrame(young_eval_y[i]))
 
-
-
+        #shuffle the dataset so that the model would be able to remember both classes
         df0_train=(pd.concat([X_train, y_train], axis=1)).sample(frac=1).reset_index(drop=True)
+        #split the new dataset into X and y: training data and desired label
         X_train = df0_train.drop(['age_new'], axis=1)
         y_train = df0_train['age_new'] 
 
+        #train the model on the portion specified for training
         model.fit(X_train, y_train.values.ravel())
+        #predict the output of X_eval
         predictions = model.predict(X_eval)
+        #Compare the obtained results to the desired ones and compute the accuracy results
         score = accuracy_score(predictions, y_eval)
         accuracys.append(score)
         print("Validation batch score: {}".format(score))
 
+    #The overall validation accuracy is the mean of the results obtained and stored in the accuracys array
     eval_accuracy = np.mean(accuracys)
 
-    #save the pretrained model:
+    #Train the model on the entire training set
     model.fit(X0_train, y0_train)
+    #save the pretrained model:
     pickle.dump(model, open(model_name, 'wb'))
 
     return eval_accuracy, model, X0_train, y0_train, X_test, y_test, k_optimal
 
 def train2(X,y, k_cross_validation_ratio, testing_size, optimal_k=True, min_range_k=1, max_range_k=100, progressObject=None, jobId=-1, model_name="pretrained_knn_model"):
     
-
-
+    #split the dataset into training and testing data
     X0_train, X_test, y0_train, y_test= train_test_split(X, y, test_size=0.2, random_state=7)
 
+    #Count the number of young and old instances and make sure that they are less than the threshold (2)
+    #If not, warn the user that this is a biased dataset
     count_young = 0
     count_old = 0
     for i in y0_train:
@@ -178,11 +205,11 @@ def train2(X,y, k_cross_validation_ratio, testing_size, optimal_k=True, min_rang
             count_old = count_old+1
 
     if (np.abs(count_young- count_old)>2):
-        warnings.warn('Error! Biased Training dataset!')
+        warnings.warn('Warning! Biased Training dataset! Your results might not be accurate.')
     
     
     eval_score_list = []
-
+    #Finding the range of the optimal k
     if optimal_k and min_range_k>0 and max_range_k>min_range_k:
         k_range= range(min_range_k, min(max_range_k, X0_train.shape[0]))
     elif optimal_k:
@@ -190,10 +217,6 @@ def train2(X,y, k_cross_validation_ratio, testing_size, optimal_k=True, min_rang
     else:
         k_range=[min_range_k]
     
-    #y0_train.value_counts()
-    
-    if(np.abs(pd.DataFrame(X0_young_train).ndim - pd.DataFrame(X0_old_train).ndim) >2 ):
-        warnings.warn('Error! Biased Training dataset!')
     
     scores = {}
     scores_list = []
@@ -209,12 +232,13 @@ def train2(X,y, k_cross_validation_ratio, testing_size, optimal_k=True, min_rang
             progressObject['jobProgress'][jobId] = min((k_range.index(k)+1.0)/len(k_range), 0.99)
     
     k_optimal = scores_list.index(max(scores_list))+min_range_k
+    #creating a model with k= optimal k
     model = KNeighborsClassifier(n_neighbors= k_optimal, weights = 'distance', algorithm='ball_tree', p=3)
     #model.fit(X0_train, y0_train)
     accuracys=[]
 
+    #For validation: repeated Stratified K-fold 
     nb_splits=10
-
     skf = StratifiedKFold(n_splits=10, random_state=None)
     skf.get_n_splits(X0_train, y0_train)
     for train_index, test_index in skf.split(X0_train, y0_train):
@@ -229,6 +253,7 @@ def train2(X,y, k_cross_validation_ratio, testing_size, optimal_k=True, min_rang
         accuracys.append(score)
         print("Validation batch score: {}".format(score))
     eval_accuracy = np.mean(accuracys)
+
     #save the pretrained model:
     model.fit(X0_train, y0_train)
     pickle.dump(model, open(model_name, 'wb'))   
@@ -236,18 +261,23 @@ def train2(X,y, k_cross_validation_ratio, testing_size, optimal_k=True, min_rang
     return eval_accuracy, model, X0_train, y0_train, X_test, y_test, k_optimal
 
 def loadPreTrained(modelName):
+    #if the pretrained model exists, load it
     if os.path.exists(modelName):
         return pickle.load(open(modelName, 'rb' ))
     else:
+        #Model not found: the user must train again
         strExcep = "Error! Pretrained model does not exist! "
         return strExcep
 
 
 def test(X_train, y_train, X_test, y_test,modelName):
+    #Load the pretrained model (or the one saved from the training phase if the user called the train method)
     model = loadPreTrained(modelName)
     model.fit(X_train, y_train)
+    #Get the calculated values
     y_pred = model.predict(X_test)
 
+    #Produce a dictionary with the precision, recall and accuracy results of every class
     classification_rep = classification_report(y_test, y_pred, output_dict=True)
     test_score = metrics.accuracy_score(y_test, y_pred)
     recall_score = metrics.recall_score(y_test, y_pred)
@@ -255,6 +285,7 @@ def test(X_train, y_train, X_test, y_test,modelName):
 
 
 def preprocessingData(file_path):
+    #Get the images from a specific path and read them by going over each and every file in the path using tqdm
     images = []
     for x, y, z in os.walk(file_path):
         for name in tqdm(z):
@@ -262,10 +293,12 @@ def preprocessingData(file_path):
     return images 
 
 def dataframeCreation(images):
+    #If the images were loaded previously and trained upon, use the saved csv file instead of loading them again
     if os.path.exists('./lbp.csv'):
         lbp_df = pd.read_csv('./lbp.csv', index_col=0)
         count = 0
     else:
+        #If the csv file is not found, the user has to load the images
         lbp_df = pd.DataFrame()
         # the parameters of the LBP algo
         # higher = more time required
@@ -274,30 +307,40 @@ def dataframeCreation(images):
         images_crop = []
         count=0
         for i in tqdm(images):
+
             if ".DS_Store" in i:
-                continue  
+                #.DS_Store is a file on mac that tqdm goes over it in case it was in the specified directory which causes 
+                #opencv to crash (no image is in this file)
+                continue
+            #read the image using opencv    
             img = cv2.imread(i)
+            #Locate the faces in the images
             faces, x,y,w,h = locateFace(img)
             if not faces == ():
+                #if a face is found, crop the image based on the faces' dimensions and coordinates extracted using the opencv  
+                #face detection classifier
                 crop_img = img[y:y+h, x:x+w]
+                #Generate the lbp samples of this image and append them to the dataframe and extract the label from the file name
                 lbp = LocalBinaryPatterns(sample_points, radius).describe(cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY))
                 row = dict(zip(range(0, len(lbp)), lbp))
                 row['ageRange'] = i.split('/')[3] ## TODO: change 3 to the index in the path where the age range is located
                 lbp_df = lbp_df.append(row, ignore_index=True)
 
             else:
+                #If no face was found in the image, an error should be raised to the user that the model can't train on such picture
                 count= count+1
                 raise ValueError('Error! No face was detected. Please try again with a clearer picture')
                  
                 
-
-        print("Count: empty: {}".format(count))
         # the age groups we decide we call 'young'
         young = ['age_10_14', 'age_15_19','age_20_24']
         # in this column, true means young, false means old
         lbp_df['age_new'] = lbp_df['ageRange'].isin(young)
+        #save the dataframe to a csv file
         lbp_df.to_csv('./lbp.csv')
 
+    #extract the young and old instances out of the dataframe and save them separately (to be used while splitting the data to 
+    # have a balanced and unbiased training dataset)
     df_young = lbp_df[lbp_df.age_new == True]
     df_old = lbp_df[lbp_df.age_new == False]
  
@@ -311,16 +354,16 @@ def dataframeCreation(images):
 
 def getRandomSamples(nbrYoung, nbrOld):
     if os.path.exists('./lbp.csv'):
+        #if the csv file is found, load it instead of loading the images all over again
         full_df = pd.read_csv('./lbp.csv', index_col=0)
-
+        #extract the young and old instances and load them separately
         youngs = full_df.loc[full_df['age_new'] == True]
         olds = full_df.loc[full_df['age_new'] == False]
-
+        #get the specified number of samples from each dataset
         random_youngs = youngs.sample(n=nbrYoung).reset_index(drop=True)
         random_olds = olds.sample(n=nbrOld).reset_index(drop=True)
-
+        #combine the results and split them into data X and label y
         full_random = random_youngs.append(random_olds)
-
         X = full_random.drop(['ageRange','age_new'], axis=1)
         y = full_random['age_new']
         return X,y
@@ -336,21 +379,24 @@ def createInputsFromImagePaths(imagePaths):
     count=0
     for i in tqdm(imagePaths):
         if ".DS_Store" in i:
+            #if this file is found, ignore it
             continue  
+        #read every image into opencv
         img = cv2.imread(i)
+        #locate the faces and extract the coordinates and crop the picture using the extracted values
         faces, x,y,w,h = locateFace(img)
         if not faces == ():
             crop_img = img[y:y+h, x:x+w]
+            #apply the lbp on every face and append the result into the created dataframe
             lbp = LocalBinaryPatterns(sample_points, radius).describe(cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY))
             row = dict(zip(range(0, len(lbp)), lbp))
             lbp_df = lbp_df.append(row, ignore_index=True)
     
         else:
+            #If no face was found, raise an error to the user 
             count= count+1
             print("Error! No face was detected. Please try again with a clearer picture")
-            #to remove the remove method   
-       
-    print("Count: empty: {}".format(count))
+
     return lbp_df
 
 def createInputFromBase64(base64Image):
@@ -409,9 +455,11 @@ def createTrainDFFromBase64(images, isYoung, progressObject, jobId):
     
 
 def predict(X, model):
+    #predict the values
     predictions = model.predict(X)
     labels = list()
     lbl = []
+    #if True then the label is Young else it is Old
     for predict in predictions:
         if predict:
             label = "Young"
@@ -424,13 +472,13 @@ def predict(X, model):
     return labels, predictions
 
 def predictFromPath(imagePath, model):
-
+    #get the data values X from the path and predict the labels
     X = createInputsFromImagePaths(imagePath)
-    # print(lbp_df)
     return predict(X, model)
 
 
 def getCurrentModel():
+    #To get the current model, we check first for the config file and return the model
     with open('./config.json') as f:
         sharedObject = json.load(f) 
     return loadPreTrained(sharedObject['model_name'])
@@ -438,6 +486,8 @@ def getCurrentModel():
 
 
 def predictFromBase64(base64Image):
+    #get the current model and extract the data values from the images 
+    #predict the labels using the predict method
     model = getCurrentModel()
     X = createInputFromBase64(base64Image)
 
@@ -566,12 +616,15 @@ def performJob(job, sharedObject):
 
 
 def predictFromMLScriptOnly(path):
+    #get the images in the specified path
     images = preprocessingData(path)
+    #Get the classes and dump them into 2 separate dataframes
     X_young_df, y_young_df ,X_old_df, y_old_df, _ =dataframeCreation(images)
-    #model = loadPreTrained(modelName="pretrained_knn_model")
+    #train the model
     eval_accuracy, model, X_train, y_train, X_test, y_test, k_optimal = train(X_young_df, y_young_df ,X_old_df, y_old_df, k_cross_validation_ratio=5, testing_size=0.2, optimal_k=True, min_range_k= 1, max_range_k=100)
-    
+    #print the nb of occurences of each class in the training dataset
     print(pd.DataFrame(y_train).apply(pd.value_counts))
+    #test on the data and get the results
     test_score, conf_rep = test(X_train, y_train,X_test, y_test, modelName="pretrained_knn_model")
     print("Test Score: {}".format(test_score))
     print(conf_rep)
@@ -581,9 +634,12 @@ def predictFromMLScriptOnly(path):
     testPath = '../dataset/male/age_10_14/pic_0126.png'
     testPath2 = "../dataset/male/age_60_94/pic_0341.png"
     patht = [testPath, testPath2]
+    #extract the images from the specified path
     X=createInputsFromImagePaths(patht)
+    #test on these pictures to predict their labels
     label , pred = predict(X, model)
     print(pred)
 
-path = "../dataset"
-predictFromMLScriptOnly(path)
+#path to be used for the prediction - to be specified in the case of using the ML script only
+"""path = "../dataset"
+predictFromMLScriptOnly(path)"""
